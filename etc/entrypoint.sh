aa6f6338ab27a4f5abf2a5bbe106d8add22345f3
theshamuel <theshamuel@gmail.com>
2017-04-28 21:56:38 +0300

    replaced test script

--- etc/entrypoint.sh
+++ etc/entrypoint.sh
@@ -5,10 +5,11 @@ echo "Configuring nginx..."
 if [ ! -f /etc/nginx/ssl/dh4096.pem ]; then
     mkdir -p /etc/nginx/ssl
     cd /etc/nginx/ssl
-#    openssl dhparam -out dh4096.pem 4096
-  #  chmod 600 dh4096.pem
+    openssl dhparam -out dh4096.pem 4096
+    chmod 600 dh4096.pem
     echo "Successful created dh4096.pem"
 fi
+
 
 # Set key paths
 echo "your domain=${DOMAIN:=$DOMAIN}"
@@ -17,23 +18,28 @@ FILE_KEY=/etc/nginx/ssl/certificates/${DOMAIN}.key
 FILE_CRT=/etc/nginx/ssl/certificates/${DOMAIN}.crt
 echo "your SSL_KEY=${FILE_KEY}"
 echo "your SSL_CRT=${FILE_CRT}"
-cp -f /etc/nginx/service-ssl.conf /etc/nginx/conf.d/service.conf
-(
+cp -f /etc/nginx/service-ssl.conf /etc/nginx/conf.d/service-ssl.conf
+
 sed -i "s|FILE_KEY|${FILE_KEY}|g" /etc/nginx/conf.d/service.conf
 sed -i "s|FILE_CRT|${FILE_CRT}|g" /etc/nginx/conf.d/service.conf
-)
+
+
+(
+while :
+do
+  sleep 10
+
+  if [ ! -f /etc/nginx/ssl/certificates/${DOMAIN}.key ]; then
+    lego -a --path=/etc/nginx/ssl --email="${EMAIL}" --domains="${DOMAIN}" --domains="www.${DOMAIN}" --http=:80 run #Generate new certificates
+  else
+    lego -a --path=/etc/nginx/ssl --email="${EMAIL}" --domains="${DOMAIN}" --domains="www.${DOMAIN}" --http=:80 --tls=:443 renew #Update certificates
+  fi
+
+  sleep 80d
+  echo "Restart nginx..."
+  nginx -s reload
+done
+)&
+
 nginx -g "daemon off;"
-nginx -s reload
-echo "Restart nginx..."
-
-
-# while :
-# do
-# done
 exit $?
-
-#lego -a --path=/etc/nginx/ssl --email="theshamuel@gmail.com" --domains="shamuel.com" --domains="www.shamuel.com" --http=:80 run
-#
-#do
-#lego -a --path=/etc/nginx/ssl --email="theshamuel@gmail.com" --domains="shamuel.com" --domains="www.shamuel.com" --http=:81 --tls=:443 renew
-#lego -a --webroot /usr/share/nginx/html --path=/etc/nginx/ssl --email="theshamuel@gmail.com" --domains="shamuel.com" --domains="www.shamuel.com" --http=:81 revoke
